09125fb75e0804043879f9e468ea3b6f52902017
Web3orNothing <leonidas0xdev1755@gmail.com>
2023-11-20 08:47:21 -0500

    optimize footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,38 +16,31 @@ const CustomButton = styled("div")({
 
 const Footer = () => {
   return (
-    <>
-      {/* <Image alt="" className="block sm:hidden" src={Pic} />
-      <Image alt="" className="hidden sm:block md:hidden" src={Pic1} />
-      <Image alt="" className="hidden md:block lg:hidden" src={Pic2} />
-      <Image alt="" className="hidden lg:block xl:hidden" src={Pic3} />
-      <Image alt="" className="hidden xl:block" src={Pic4} /> */}
-      <div
-        className="bg-cover h-[42px]"
-        style={{ backgroundImage: `url(${FooterBg.src})` }}
-      >
-        <div className="hidden sm:flex gap-1 justify-center text-white text-[12px] py-2">
-          <CustomText>Hit</CustomText>
-          <CustomButton>R</CustomButton>
-          <CustomButton>R</CustomButton>
-          <CustomButton>R</CustomButton>
-          <CustomButton>R</CustomButton>
-          <CustomText>to switch tabs &bull;</CustomText>
-          <CustomButton style={{ display: "flex", alignItems: "center" }}>
-            <Image alt="" width={12} src={LoopedSquare} />
-          </CustomButton>
-          <CustomButton>K</CustomButton>
-          <CustomText>to type &bull;</CustomText>
-          <CustomButton>enter</CustomButton>
-          <CustomText>to go &bull;</CustomText>
-          <CustomButton style={{ display: "flex", alignItems: "center" }}>
-            <Image alt="" width={12} src={LoopedSquare} />
-          </CustomButton>
-          <CustomButton>enter</CustomButton>
-          <CustomText>to execute</CustomText>
-        </div>
+    <div
+      className="bg-cover h-[42px]"
+      style={{ backgroundImage: `url(${FooterBg.src})` }}
+    >
+      <div className="hidden sm:flex gap-1 justify-center text-white text-[12px] py-2">
+        <CustomText>Hit</CustomText>
+        <CustomButton>R</CustomButton>
+        <CustomButton>R</CustomButton>
+        <CustomButton>R</CustomButton>
+        <CustomButton>R</CustomButton>
+        <CustomText>to switch tabs &bull;</CustomText>
+        <CustomButton style={{ display: "flex", alignItems: "center" }}>
+          <Image alt="" width={12} src={LoopedSquare} />
+        </CustomButton>
+        <CustomButton>K</CustomButton>
+        <CustomText>to type &bull;</CustomText>
+        <CustomButton>enter</CustomButton>
+        <CustomText>to go &bull;</CustomText>
+        <CustomButton style={{ display: "flex", alignItems: "center" }}>
+          <Image alt="" width={12} src={LoopedSquare} />
+        </CustomButton>
+        <CustomButton>enter</CustomButton>
+        <CustomText>to execute</CustomText>
       </div>
-    </>
+    </div>
   );
 };
 
